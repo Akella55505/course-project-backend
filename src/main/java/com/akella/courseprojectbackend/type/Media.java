285ad95bfeabda89925d51a5f6ab0d76129d0d88
Akella <leosaz0617@gmail.com>
2025-11-21 13:54:01 +0200

    feat: return non-null

--- src/main/java/com/akella/courseprojectbackend/type/Media.java
+++ src/main/java/com/akella/courseprojectbackend/type/Media.java
@@ -1,6 +1,8 @@
 package com.akella.courseprojectbackend.type;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.ArrayList;
 
-public record Media(ArrayList<String> photos, ArrayList<String> videos)  {
-}
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record Media(ArrayList<String> photos, ArrayList<String> videos) {}
