afee980fb13e939e7c17d3166f061d9e1092903b
Akella <leosaz0617@gmail.com>
2025-11-22 11:56:27 +0200

    chore: code cleanup

--- src/main/java/com/akella/courseprojectbackend/config/WebConfig.java
+++ src/main/java/com/akella/courseprojectbackend/config/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     private final DataSourceInterceptor dataSourceInterceptor;
 
     public WebConfig(DataSourceInterceptor dataSourceInterceptor) {
